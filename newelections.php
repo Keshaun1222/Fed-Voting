a8e205e1db773e5de56d8e25921f33d899b5811f
Keshaun <keshaun@eotir.com>
2016-08-03 21:27:52 -0400

    Fixed error in index.php

--- newelections.php
+++ newelections.php
@@ -1,4 +1,4 @@
 <?php
 require_once 'config.php';
 
-$mysqli->query("INSERT INTO elections VALUE (NULL, 1, '2016-08-01 6:00:00', '2016-08-03 6:00:00')");
+$mysqli->query("INSERT INTO elections VALUE (NULL, 1, '2016-08-01 18:00:00', '2016-08-03 18:00:00')");
