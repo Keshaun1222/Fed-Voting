509cd4cd778615ef3a4f8541542e937c1bc55f2a
Keshaun <keshaun@eotir.com>
2016-08-03 21:43:55 -0400

    Fixed error in index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ print_r($citizen);
 echo "\r\n";
 //if ($citizen['party']['id'] == $fedsID) echo 'A FED!!!!';*/
 $date = date("Y-m-d H:i:s", time() - (60 * 60 * 2));
-$query = $mysqli->query("SELECT * FROM elections WHERE startTime < CURRENT_TIMESTAMP  AND endTime > $date");
+$query = $mysqli->query("SELECT * FROM elections WHERE startTime < $date  AND endTime > $date");
 ?>
 <!DOCTYPE html>
 <html>
